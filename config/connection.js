9137bff07b50573e1eb39dc40cab008bed93e165
Shivam Parekh <shivam@knights.ucf.edu>
2017-02-11 06:35:11 -0500

    updated index

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,5 @@
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-} else {
-
   connection = mysql.createConnection({
 
     host: "localhost",
